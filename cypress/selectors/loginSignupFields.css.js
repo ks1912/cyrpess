348f790b7a278a5be17d22bc8df6b0f21dfd4783
kunalSinghQA <kunal.s@feuji.com>
2023-12-13 10:51:24 +0530

    updating base code

---
+++ cypress/selectors/loginSignupFields.css.js
@@ -0,0 +1,5 @@
+module.exports = {
+  loginPageUserName: "//span[contains(text(),'Username')]",
+  loginPagePassword: "//span[contains(text(),'Username')]",
+  loginButton: "//form//span[contains(text(),'Login')]",
+};
